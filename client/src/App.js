ac33fe1ef9ce2f5e11752f69df442d5565a9665d
Marie Torbay <marie.torbay@gmail.com>
2022-04-21 20:20:15 +0200

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -8,95 +8,17 @@ import SignInModal from './components/SignInModal'
 import SignUpModal from './components/SignUpModal'
 import SearchBook from './pages/SearchBook'
 import CurrentLoans from './pages/Private/CurrentLoans/CurrentLoans'
+import Contact from './pages/Contact'
 
 
 function App() {
-
-  const [books, setBooks] = useState([])
-
-///users/update/626041d878521e4cb878b435
-
-  // useEffect(() => {
-
-  //   const updateUser = async () => {
-  //     await fetch('/users/update/625ffe668edb6fa8f00d76c6', {
-  //       headers: {
-  //         Accept: 'application/json',
-  //         'Content-Type' : 'application/json'
-  //       },
-  //       method: 'PATCH',
-  //       body: JSON.stringify({   
-  //         userName: "Coulibali",
-  //         userSurname: "Torbay",
-  //         userFirebaseID: "jreiozhrieoz",
-  //         userLoans: [
-  //           {
-  //               googleId: "livre1",
-  //               loanDate: "2012-04-21T18:25:43-05:00",
-  //               endLoanDate: "2022-04-21T18:25:43-05:00",
-  //               loanRenewed: false
-  //           },
-  //           {
-  //               googleId: "livre1",
-  //               loanDate: "2002-04-21T18:25:43-05:00",
-  //               endLoanDate: "2012-04-21T18:25:43-05:00",
-  //               loanRenewed: false
-  //           }
-  //       ]
-  //   })
-  //   })
-  // }
-    
-  // updateUser()
-
-  //   const fetchData = async () => {
-  //     const data = await fetch('/users/')
-  //     const json = await data.json()
-  //     setBooks(json)
-  //   }
-
-  //   fetchData()
-
-  // const addBook = async () => {
-  //     await fetch('/users/', {
-  //       headers: {
-  //         Accept: 'application/json',
-  //         'Content-Type' : 'application/json'
-  //       },
-  //       method: 'POST',
-  //       body: JSON.stringify({   
-  //         userName: "Coulibali",
-  //         userSurname: "Torbay",
-  //         userFirebaseID: "jreiozhrieoz",
-  //         userLoans: [
-  //             {
-  //                 googleId: "livre1",
-  //                 loanDate: "2012-04-21T18:25:43-05:00",
-  //                 endLoanDate: "2022-04-21T18:25:43-05:00",
-  //                 loanRenewed: false
-  //             },
-  //             {
-  //                 googleId: "livre1",
-  //                 loanDate: "2002-04-21T18:25:43-05:00",
-  //                 endLoanDate: "2012-04-21T18:25:43-05:00",
-  //                 loanRenewed: false
-  //             }
-  //         ]
-  //     })
-  //     })
-  //   }
-
-  //   // addBook()
-  // }, [])
-
-  // console.log(books)
   
   return (
     <>
       <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path='/contact' element={<h1 className='mt-4'>Contact</h1>}/>
+        <Route path='/contact' element={<Contact/>}/>
         <Route path='/rechercher' element={<SearchBook/>}/>
         <Route path="/private" element={<Private />}>
           <Route path="/private/private-home" element={<PrivateHome />} />
---
+++ client/src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div className="container">
+        <h1 className='mt-4'>Contact</h1>
+        <h4><strong>Vous pouvez nous contacter au : </strong> 01 23 45 67 89</h4>
+        <h4><strong>Ou bien par email : </strong> bibliotheque@pontault-combault.fr</h4>
+    </div>
+  )
+}
