4a06219644d07d49187a66aadc7099c01a3c7bb1
Marie Torbay <marie.torbay@gmail.com>
2022-04-21 23:04:02 +0200

    changed firebase-config

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyAqo3VW-HKdTHgzXdhsdNIf0j0xieq1Qaw",
+  authDomain: "appli-biblio-study.firebaseapp.com",
+  projectId: "appli-biblio-study",
+  storageBucket: "appli-biblio-study.appspot.com",
+  messagingSenderId: "237707371714",
+  appId: "1:237707371714:web:c347f0179b5845fe59475b"
 };
 
 const app = initializeApp(firebaseConfig);
